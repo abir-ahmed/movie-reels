abd9e2dcf3d46ddb37412629b52ae4a287aeb690
Abir Ahmed <abir.ahmed@bjitgroup.com>
2024-03-22 15:04:08 +0600

    Movie_Reels: Add 'Go Back' Button to Search Page

--- lib/searchMovies.dart
+++ lib/searchMovies.dart
@@ -9,19 +9,34 @@ class SearchMovieScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Search Movie'),
       ),
-      body: Center(
-        child: MaterialButton(
-          color: Colors.pink,
-          child: Text(
-            'Go back',
-            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.purple, Colors.red])),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+          child: Column(
+            children: [
+              Center(
+                child: MaterialButton(
+                  color: Colors.pink,
+                  child: Text(
+                    'Go back',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {
+                    // Navigate to the home screen when tapped.
+                    // Navigate back to the first screen by popping the current route
+                    // off the stack.
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+            ],
           ),
-          onPressed: () {
-            // Navigate to the home screen when tapped.
-            // Navigate back to the first screen by popping the current route
-            // off the stack.
-            Navigator.pop(context);
-          },
         ),
       ),
     );
